1926d36a8dc9b2e8ea26fa347ad11e96850c04ae
70kg111 <76104718@qq.com>
2019-07-17 15:24:07 +0800

    加入icon

--- src/button.vue
+++ src/button.vue
@@ -1,11 +1,21 @@
 <template>
-  <button class="g-button">提交</button>
+  <div>
+    <!-- 按钮中的提交两个字是传入进去的，所以不直接写，在index.html中的内容会显示到slot标签中，vue的特性 -->
+    <button class="g-button">
+      <!-- 判断一下，如果用户不传参的话就不显示这个svg -->
+      <svg v-if="icon" class="icon">
+        <!-- 下面的最终效果是 <use xlink:href="#i-settings" />，这里的icon与下面props传入的值对应 -->
+        <use :xlink:href="`#i-${icon}`" />
+      </svg>
+      <slot></slot>
+    </button>
+  </div>
 </template>
 
 
 <script>
 export default {
-  name: "Button"
+  props: ["icon"]
 };
 </script>
 
@@ -20,6 +30,10 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  //   万金油居中代码
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
 
   /*&符号表示当前的选择器，scss语法*/
   &:hover {
